455e375c4d1137849bf6df741a73b769a2fb8081
Kalany <duncan.miard@outlook.fr>
2025-06-19 23:08:27 +0200

    Correction des bugs

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -30,13 +30,16 @@ export const DataProvider = ({ children }) => {
     if (data) return;
     getData();
   });
-  
+
+  const last = data?.events?.[0] || null;
+
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         data,
         error,
+        last,
       }}
     >
       {children}
@@ -46,7 +49,7 @@ export const DataProvider = ({ children }) => {
 
 DataProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export const useData = () => useContext(DataContext);
 
